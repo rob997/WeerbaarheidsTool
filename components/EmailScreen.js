478c1e9de5a81559b2beafd27a00d4579771c906
Rob <robperdok@hotmail.com>
2022-06-08 19:24:45 +0200

    classes to functions

--- components/EmailScreen.js
+++ components/EmailScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import {
   View,
   Text,
@@ -9,51 +9,41 @@ import {
 } from "react-native";
 import styles from "./styles/styles.js";
 
-export default class EmailScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: "",
-    };
-  }
-  submit() {
-    console.warn(this.state);
-  }
-  render() {
-    return (
-      <SafeAreaView style={styles.mainview}>
-        <View style={styles.titleview}>
-          <Text style={styles.title}>
-            Hier kunt u testen of uw e-mailadres is getroffen door een hack op
-            een database.
-          </Text>
-        </View>
-        <View style={styles.inputview}>
-          <Text>Vul hier uw email in:</Text>
-          <View style={styles.inputwrapper}>
-            <TextInput
-              placeholder="email@domain.com"
-              onChangeText={(text) => {
-                this.setState({ email: text });
+export default function EmailScreen() {
+  const [email, setEmail] = useState("");
+  return (
+    <SafeAreaView style={styles.mainview}>
+      <View style={styles.titleview}>
+        <Text style={styles.title}>
+          Hier kunt u testen of uw e-mailadres is getroffen door een hack op een
+          database.
+        </Text>
+      </View>
+      <View style={styles.inputview}>
+        <Text>Vul hier uw email in:</Text>
+        <View style={styles.inputwrapper}>
+          <TextInput
+            placeholder="email@domain.com"
+            onChangeText={(text) => {
+              setEmail({ text });
+            }}
+            style={styles.input}
+          ></TextInput>
+          <View>
+            <Button
+              title="Check"
+              onPress={() => {
+                console.warn(email);
               }}
-              style={styles.input}
-            ></TextInput>
-            <View>
-              <Button
-                title="Check"
-                onPress={() => {
-                  this.submit();
-                }}
-                color="green"
-              />
-            </View>
+              color="green"
+            />
           </View>
         </View>
+      </View>
 
-        <View style={styles.resultview}>
-          <Text>Resultaten:</Text>
-        </View>
-      </SafeAreaView>
-    );
-  }
+      <View style={styles.resultview}>
+        <Text>Resultaten:</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
--- components/InfoScreen.js
+++ components/InfoScreen.js
@@ -2,33 +2,31 @@ import React, { Component } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import styles from "./styles/styles.js";
 
-export default class InfoScreen extends Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.mainview}>
-        <View style={styles.titleview}>
-          <Text
-            onPress={() => navigation.navigate("E-mail")}
-            style={styles.title}
-          >
-            Info Scherm
-          </Text>
+export default function InfoScreen() {
+  return (
+    <SafeAreaView style={styles.mainview}>
+      <View style={styles.titleview}>
+        <Text
+          onPress={() => navigation.navigate("E-mail")}
+          style={styles.title}
+        >
+          Info Scherm
+        </Text>
+      </View>
+
+      <View style={styles.infoviewcontainer}>
+        <View style={styles.infoview}>
+          <Text>Tip 1:</Text>
         </View>
 
-        <View style={styles.infoviewcontainer}>
-          <View style={styles.infoview}>
-            <Text>Tip 1:</Text>
-          </View>
-
-          <View style={styles.infoview}>
-            <Text>Tip 2:</Text>
-          </View>
-
-          <View style={styles.infoview}>
-            <Text>Tip 3:</Text>
-          </View>
+        <View style={styles.infoview}>
+          <Text>Tip 2:</Text>
         </View>
-      </SafeAreaView>
-    );
-  }
+
+        <View style={styles.infoview}>
+          <Text>Tip 3:</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
