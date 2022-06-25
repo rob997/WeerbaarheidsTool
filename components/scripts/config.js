90ba4d48a372a2c75d53caa683118b1337189bfe
Rob <robperdok@hotmail.com>
2022-06-25 16:15:27 +0200

    ip update

--- components/scripts/config.js
+++ components/scripts/config.js
@@ -1,2 +1,2 @@
-// IP address of the Node.js server ( example: 'http://http://20.68.112.139:3000' )
-export default ipadress = "http://20.90.20.91:3000";
+// IP address of the Node.js server ( example: 'http://20.68.112.139:3000' )
+export default ipadress = "http://51.141.74.138:3000";
